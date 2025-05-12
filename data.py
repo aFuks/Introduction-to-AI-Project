d753d00a3928ee18ecb747c8ad478fa3e13f45a6
aleksanderfuks <s188554@student.pg.edu.pl>
2025-05-12 13:25:05 +0200

    uzupełnienie brakujących danych

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 from ucimlrepo import fetch_ucirepo 
+import pandas as pd
   
 # fetch dataset 
 lung_cancer = fetch_ucirepo(id=62) 
@@ -6,10 +7,15 @@ lung_cancer = fetch_ucirepo(id=62)
 # data (as pandas dataframes) 
 X = lung_cancer.data.features 
 y = lung_cancer.data.targets 
-  
-# metadata 
-print(lung_cancer.metadata) 
-  
-# variable information 
-print(lung_cancer.variables) 
+
+# uzupełnienie brakujących wartości medianią 
+X["Attribute4"] = X["Attribute4"].fillna(X["Attribute4"].median())
+X["Attribute38"] = X["Attribute38"].fillna(X["Attribute38"].median())
+
+# Połączenie danych
+df = pd.concat([X, y], axis=1)
+
+df.to_csv('lung_cancer_dataset_filled.csv', index=False)
+
+print("Dane zapisane")
 
